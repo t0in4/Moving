f76e1a3444492193d7e898643dd4e60b07e0c672
t0in4 <t0in4@yahoo.com>
2023-02-08 15:14:23 +0900

    Initial commit

---
+++ src/moving/Movable.java
@@ -0,0 +1,14 @@
+package moving;
+
+interface Movable {
+    default void stepAhead(String[][] x, String y){
+
+    };
+
+    void turnLeft();
+    void turnRight();
+    default void turnAround() {
+        turnLeft();
+        turnLeft();
+    }
+}
